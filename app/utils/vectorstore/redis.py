9a927acb930928ed7be85147c0b594cb9d4693ef
Andy He <andy.he@avanade.com>
2023-09-26 15:53:00 +0800

    finish similarity search

--- app/utils/vectorstore/redis.py
+++ app/utils/vectorstore/redis.py
@@ -2,17 +2,20 @@ from langchain.docstore.document import Document
 from langchain.vectorstores.base import VectorStoreRetriever
 from langchain.vectorstores.redis import Redis as Redis_TYPE
 from langchain.embeddings.base import Embeddings
+from langchain.vectorstores.redis import RedisText
 
 import logging
+import numpy as np
 from app.config import Config
 from app.utils.vectorstore import BaseVectorStore
 
+import hashlib
 from redis.client import Redis
 from redis.commands.search.query import Query
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from redis.commands.search.field import VectorField, TagField, TextField
 
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Dict, Tuple
 
 logger = logging.getLogger(__name__)
 
@@ -124,7 +127,7 @@ class RedisExtended(BaseVectorStore):
 
         return RedisCluster.from_url(redis_url, **kwargs)
 
-    def create_index(self, index_name: str, distance_metric: Optional[str]="COSINE") -> None:
+    def create_index(self, index_name: str, distance_metric: Optional[str]="COSINE", metadata_schema: Dict[str, str]=None) -> None:
         """This function creates an index.
         
         Args:
@@ -135,19 +138,28 @@ class RedisExtended(BaseVectorStore):
         """
 
         content = TextField(name="content")
-        metadata = TextField(name="metadata")
         content_vector = VectorField("content_vector",
                     "HNSW", {
                         "TYPE": "FLOAT32",
-                        "DIM": 1536,
+                        "DIM": self.config.OPENAI_EMBEDDING_SIZE,
                         "DISTANCE_METRIC": distance_metric,
                         "INITIAL_CAP": 1000,
                     })
         
+        fields = [content, content_vector]
+
+        if metadata_schema is not None:
+            for key, value in metadata_schema.items():
+                if value.lower() == 'text':
+                    fields.append(TextField(name=key))
+                else:
+                    raise ValueError(f"Metadata schema value '{value}' is not supported.")
+        
         self.redis_client.ft(format_index_name(index_name)).create_index(
-            fields = [content, metadata, content_vector],
+            fields = fields,
             # TODO: Langchain use this as the default prefix
-            definition = IndexDefinition(prefix=["doc"], index_type=IndexType.HASH)
+            # definition = IndexDefinition(prefix=["doc"], index_type=IndexType.HASH)
+            definition = IndexDefinition(prefix=[format_index_name(index_name)], index_type=IndexType.HASH)
         )
 
     def drop_index(self, index_name: str) -> None:
@@ -175,21 +187,23 @@ class RedisExtended(BaseVectorStore):
             return True
         except:
             return False
-        
-    def _get_langchain_redis(self, index_name: str = None) -> Redis_TYPE:
-        """This function gets the langchain redis.
+
+    def _get_redis_schema(self, index_name: str = None) -> Dict[str, str]:
+        """This function gets the redis schema.
         
         Args:
             index_name: the index name
-        Returns:  
-            the langchain redis
-        """
+        Returns:
+            the redis schema
+        
+        """    
+
         # Check if the index exists
         if not self.check_existing_index(index_name):
             raise ValueError(f"Index '{index_name}' does not exist.")
         
         index_info = self.redis_client.ft(format_index_name(index_name)).info()
-        logger.info(f"Index info: {index_info}")
+        logger.debug(f"Index info: {index_info}")
 
         schema = {}
         # Langchain Redis requires the schema to be defined
@@ -209,7 +223,21 @@ class RedisExtended(BaseVectorStore):
 
             schema[identifier] = data_type
         
-        logger.info(f"Schema: {schema}")
+        logger.debug(f"Schema: {schema}")
+
+        return schema
+
+    
+    def _get_langchain_redis(self, index_name: str = None) -> Redis_TYPE:
+        """This function gets the langchain redis.
+        
+        Args:
+            index_name: the index name
+        Returns:  
+            the langchain redis
+        """
+        
+        schema = self._get_redis_schema(index_name)
 
         # Create langchain Redis from existing index
         redis_langchain = Redis_TYPE.from_existing_index(
@@ -239,11 +267,142 @@ class RedisExtended(BaseVectorStore):
         """
 
         redis_langchain = self._get_langchain_redis(index_name)
+        logger.debug(f"Schemas: {redis_langchain.schema}")
+
+        keys = []
+        for document in documents:
+            key = hashlib.sha256(document.page_content.encode("utf-8")).hexdigest()
+            keys.append(f"{format_index_name(index_name)}:{key}")
 
         # Add documents to the index
-        redis_langchain.add_documents(documents)
+        redis_langchain.add_documents(documents, keys=keys)
 
-    def get_retriever(self, index_name: str | None = None) -> VectorStoreRetriever:
+    def add_texts(
+            self, 
+            texts: List[str],
+            metadatas: Optional[List[Dict[str, Any]]] = None, 
+            index_name: str = None, 
+            **kwargs: Any
+        ) -> None:
+        """This function adds texts to the vector store.
+        
+        Args:
+            texts: the texts to add
+            index_name: the index name
+        Returns:
+            none
+        """
+
+        redis_langchain = self._get_langchain_redis(index_name)
+
+        keys = []
+        for text in texts:
+            key = hashlib.sha256(text.encode("utf-8")).hexdigest()
+            keys.append(f"{format_index_name(index_name)}:{key}")
+
+        # Add texts to the index
+        redis_langchain.add_texts(texts, metadatas=metadatas, keys=keys)
+
+    def similarity_search( 
+            self, 
+            query: str, 
+            k: int = 4, 
+            filter: Optional[Dict[str, Any]] = None,
+            index_name: Optional[str] = None
+        ) -> List[Tuple[Document, float]]:
+        """This function performs a similarity search.
+        
+        Args:
+            query: the query
+            k: the number of results
+            filter: the filter
+        Returns:
+            docs and relevance scores in the range [0, 1].
+        """
+        
+        ## It is too buggy to use langchain redis similarity search
+        ## We decide to use the native redis similarity search instead
+
+        # redis_langchain = self._get_langchain_redis(index_name)
+
+        # logger.debug(f"Schemas: {redis_langchain.schema}")
+
+        # filter_expression = None
+
+        # for key, value in filter.items():            
+        #     # TODO: only text equality is supported for now
+        #     if filter_expression is None:
+        #         filter_expression = RedisText(key) == value
+        #     else:
+        #         filter_expression &= RedisText(key) == value
+
+        # return redis_langchain.similarity_search_with_relevance_scores(query, k, filter=filter_expression)
+
+        schema = self._get_redis_schema(index_name)
+        
+        query, query_params = self._contruct_redis_query(question=query, k=k, filter=filter)
+
+        results = self.redis_client.ft(format_index_name(index_name)).search(query, query_params = query_params)
+
+        docs_with_scores: List[Tuple[Document, float]] = []
+        for result in results.docs:
+            metadata = {}
+            metadata = {"id": result.id}
+            
+            for key in schema.keys():
+                # TODO: Dont return the content and content vector, currently it is hard coded
+                if key != "content_vector" or key != "content":
+                    metadata[key] = result.__getattribute__(key)
+            doc = Document(page_content=result.content, metadata=metadata)
+
+            docs_with_scores.append((doc, result.score))
+
+        logger.debug(f"docs_with_scores: {docs_with_scores}")
+
+        return docs_with_scores
+
+    def _contruct_redis_query(self, question: str, k: int = 4, filter: Optional[Dict[str, Any]] = None) -> Tuple[Query, Dict[str, Any]]:
+        """This function constructs a redis query.
+        
+        Args:
+            question: the question
+            filter: the filter
+        Returns:
+            the redis query
+        """
+
+        emdeded_question = self.embeddings.embed_query(question)
+
+        if filter is not None:
+            filter_expression = []
+            for key, value in filter.items():
+                filter_expression.append(f"@{key}:{value}")
+
+            filter_expression = " ".join(filter_expression)
+
+            # Langchain use content_vector as the default vector name
+            query = (
+                Query(f"({filter_expression})=>[KNN {k} @content_vector $vec as score]")
+                .sort_by("score")
+                .paging(0, 2)
+                .dialect(2)
+            )
+
+        else:
+            # Langchain use content_vector as the default vector name
+            query = (
+                Query(f"*=>[KNN {str(k)} @content_vector $vec as score]")
+                .sort_by("score")
+                .paging(0, 2)
+                .dialect(2)
+            )
+
+        query_params = {'vec': np.array(emdeded_question).astype(np.float32).tobytes()}
+            
+        return query, query_params
+
+
+    def get_retriever(self, index_name: Optional[str] = None) -> VectorStoreRetriever:
         """This function gets a retriever.
         
         Args:
--- tests/unit/utils/vectorstore/test_vectorstore.py
+++ tests/unit/utils/vectorstore/test_vectorstore.py
@@ -99,64 +99,78 @@ def test_add_documents(vector_store):
 
     for key, vector_store in vector_store.items():
         if key == 'faiss':
-            continue
             vector_store.add_documents([doc])
 
         elif key == 'redis':
-            # vector_store.create_index('test_add_document_in_unit_test')
+            metadata_schema = {"source": "text"}
+            vector_store.create_index('test_add_document_in_unit_test', metadata_schema=metadata_schema)
             vector_store.add_documents([doc], index_name='test_add_document_in_unit_test')
+            vector_store.drop_index('test_add_document_in_unit_test')
 
-# def test_add_texts(vector_store):
-#     """This function tests add texts function for vector store."""
+def test_add_texts(vector_store):
+    """This function tests add texts function for vector store."""
 
-#     text = "This is a test document only."
-#     metadata = {"source": "local"}
+    text = "This is a test document only."
+    metadata = {"source": "local"}
 
-#     for key, vector_store in vector_store.items():
-#         if key == 'faiss':
-#             vector_store.add_texts([text], [metadata])
+    for key, vector_store in vector_store.items():
+        if key == 'faiss':
+            vector_store.add_texts([text], [metadata])
 
-# def test_similarity_search(vector_store):
-#     """This function tests similarity search function for vector store."""
+def test_similarity_search(vector_store):
+    """This function tests similarity search function for vector store."""
 
-#     query = "This is a test query only."
-#     doc1 =  Document(page_content="This is a test document from local.", metadata={"source": "local"})
-#     # doc2 =  Document(page_content="This is a test document from web.", metadata={"source": "web"})
+    query = "This is a test query only."
+    doc1 =  Document(page_content="This is a test document from local.", metadata={"source": "local"})
 
-#     for key, vector_store in vector_store.items():
-#         if key == 'faiss':
-#             vector_store.add_documents([doc1])
-#             vector_store.add_texts(["This is a test document from web."], [{"source": "web"}])
-#             result = vector_store.similarity_search(query, filter={"source": "web"})
+    for key, vector_store in vector_store.items():
+        if key == 'faiss':
+            vector_store.add_documents([doc1])
+            vector_store.add_texts(["This is a test document from web."], [{"source": "web"}])
+            result = vector_store.similarity_search(query, filter={"source": "web"})
 
-#             assert len(result) > 0
-#             assert result[0][0].page_content == "This is a test document from web."
-#             assert result[0][0].metadata["source"] == "web"
-
-# def test_get_retiever(vector_store):
-#     """This function tests get retiever function for vector store."""
-
-#     text_splitter = TokenTextSplitter(chunk_size=2000, chunk_overlap=500)
-#     document_A = TextLoader('samples/A.txt', encoding = 'utf-8').load()
-#     document_B = TextLoader('samples/B.txt', encoding = 'utf-8').load()
-#     document_C = TextLoader('samples/C.txt', encoding = 'utf-8').load()
-
-#     documents = [document_A, document_B, document_C]
-#     query = "Who is Elon Musk?"
-
-#     for key, vector_store in vector_store.items():
-#         if key == 'faiss':
-#             for doc in documents:
-#                 chunks = text_splitter.split_documents(doc)
-#                 vector_store.add_documents(chunks)
-
-#             retriver = vector_store.get_retriever()
-
-#             result = retriver.get_relevant_documents(query)
-
-#             assert len(result) > 0
+            assert len(result) > 0
+            assert result[0][0].page_content == "This is a test document from web."
+            assert result[0][0].metadata["source"] == "web"
             
-#             logger.info(result[0].page_content)
+        
+        elif key == 'redis':
+            metadata_schema = {"source": "text"}
+            vector_store.create_index('test_similarity_search_in_unit_test', metadata_schema=metadata_schema)
+            vector_store.add_documents([doc1], index_name='test_similarity_search_in_unit_test')
+            vector_store.add_texts(["This is a test document from web."], [{"source": "web"}], index_name='test_similarity_search_in_unit_test')
+            result = vector_store.similarity_search(query, filter={"source": "web"}, index_name='test_similarity_search_in_unit_test')
+
+            assert len(result) > 0
+            assert result[0][0].page_content == "This is a test document from web."
+            assert result[0][0].metadata["source"] == "web"
+
+            vector_store.drop_index('test_similarity_search_in_unit_test')
+
+def test_get_retiever(vector_store):
+    """This function tests get retiever function for vector store."""
+
+    text_splitter = TokenTextSplitter(chunk_size=2000, chunk_overlap=500)
+    document_A = TextLoader('samples/A.txt', encoding = 'utf-8').load()
+    document_B = TextLoader('samples/B.txt', encoding = 'utf-8').load()
+    document_C = TextLoader('samples/C.txt', encoding = 'utf-8').load()
+
+    documents = [document_A, document_B, document_C]
+    query = "Who is Elon Musk?"
+
+    for key, vector_store in vector_store.items():
+        if key == 'faiss':
+            for doc in documents:
+                chunks = text_splitter.split_documents(doc)
+                vector_store.add_documents(chunks)
+
+            retriver = vector_store.get_retriever()
+
+            result = retriver.get_relevant_documents(query)
+
+            assert len(result) > 0
+            
+            logger.info(result[0].page_content)
 
 
 def test_check_existing_index(vector_store):
